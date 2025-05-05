5d622012da949d17e05aab3f245a0bbf6e5f95c7
WhaleRed <redwhalecs@gmail.com>
2025-05-05 20:58:43 +0800

    Edit for borrowed, replaced and returned

--- Application/src/edit.py
+++ Application/src/edit.py
@@ -12,7 +12,7 @@ db = mysql.connector.connect(
 )
 
 #Borrower Array values must have
-# NewID, NewProfID, Fname, Lname, Program, Block, CurrentID
+# NewID, NewProfID, NewFname, NewLname, NewProgram, NewBlock, CurrentID
 def editBorrower(borrower):
   mycursor = db.cursor()
   mycursor.execute("UPDATE borrower SET BorrowerID = %s, ProfessorID = %s, FirstName = %s, LastName = %s, Program = %s, Block = %s WHERE BorrowerID = %s", borrower)
@@ -20,7 +20,7 @@ def editBorrower(borrower):
   mycursor.close()
 
 #Prof Array values must have
-#NewProfID, Fname, Lname, CurrentProfID
+#NewProfID, NewFname, NewLname, CurrentProfID
 def editProfessor(professor):
   mycursor = db.cursor()
   mycursor.execute("UPDATE professor SET  ProfessorID = %s, FirstName = %s, LastName = %s WHERE ProfessorID =%s", professor)
@@ -28,9 +28,31 @@ def editProfessor(professor):
   mycursor.close()
 
 #Equipment Array values must have
-#NewEquipmentID, Name, Quantity, Category, CurrentEquipmentID
+#NewEquipmentID, NewName, NewQuantity, NewCategory, CurrentEquipmentID
 def editEquipment(equipment):
   mycursor = db.cursor()
   mycursor.execute("UPDATE equipment SET EquipmentID = %s, Name = %s, Quantity = %s, Category = %s WHERE EquipmentID = %s", equipment)
+  db.commit()
+  mycursor.close()
+
+#Equipment Array values must have
+#NewEquipmentID, NewBorrowerID, NewBorrow_date, CurrentEquipmentID, CurrentBorrowerID, CurrentBurror_date
+def editBorrowedEquipment(equipment):
+  mycursor = db.cursor()
+  mycursor.execute("UPDATE borrowed_equipment SET EquipmentID = %s, BorrowerID = %s, Borrow_date = %s WHERE (EquipmentID = %s AND BorrowerID = %s AND Borrow_date = %s)", equipment)
+  db.commit()
+  mycursor.close()
+
+#Equipment Array values must have
+#NewEquipmentID, NewBorrowerID, NewReplacement_date, CurrentEquipmentID, CurrentBorrowerID, CurrentReplaced_date
+def editReplacedEquipment(equipment):
+  mycursor = db.cursor()
+  mycursor.execute("UPDATE replaced_equipment SET EquipmentID = %s, BorrowerID = %s, Replacement_date = %s WHERE (EquipmentID = %s AND BorrowerID = %s AND Replacement_date = %s)", equipment)
+  db.commit()
+  mycursor.close()
+
+def editReturnedEquipment(equipment):
+  mycursor = db.cursor()
+  mycursor.execute("UPDATE returned_equipment SET EquipmentID = %s, BorrowerID = %s, Return_date = %s, Status = %s WHERE (EquipmentID = %s AND BorrowerID = %s AND Return_date = %s)", equipment)
   db.commit()
   mycursor.close()
