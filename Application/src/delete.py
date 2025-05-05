1cd3d5f794ba6c4c36f7b277a56b1c7d2a980363
WhaleRed <redwhalecs@gmail.com>
2025-05-05 20:58:08 +0800

    Delete for borrowed, replaced and returned

--- Application/src/delete.py
+++ Application/src/delete.py
@@ -11,6 +11,7 @@ db = mysql.connector.connect(
   database= os.getenv("DATABASE_NAME")
 )
 
+#Pass idnum
 def delBorrower(idnum):        
 
   mycursor = db.cursor()
@@ -19,6 +20,7 @@ def delBorrower(idnum):
   db.commit()
   mycursor.close()
 
+#Pass idnum
 def delProfessor(idnum):        
 
   mycursor = db.cursor()
@@ -27,10 +29,35 @@ def delProfessor(idnum):
   db.commit()
   mycursor.close()
 
+#Pass idnum
 def delEquipment(idnum):        
 
   mycursor = db.cursor()
 
   mycursor.execute("DELETE FROM equipment WHERE EquipmentID = %s", (idnum,))
+  db.commit()
+  mycursor.close()
+
+#Pass multiple values since compounded PK
+def delBorrowedEquipment(equipmentID, borrowerID, borrowDate):
+  mycursor = db.cursor()
+
+  mycursor.execute("DELETE FROM borrowed_equipment WHERE (EquipmentID = %s AND BorrowerID = %s AND Borrow_date = %s)", (equipmentID, borrowerID, borrowDate))
+  db.commit()
+  mycursor.close()
+
+#Pass multiple values since compounded PK
+def delReplacedEquipment(equipmentID, borrowerID, replacementDate):
+  mycursor = db.cursor()
+
+  mycursor.execute("DELETE FROM replaced_equipment WHERE (EquipmentID = %s AND BorrowerID = %s AND Replacement_date = %s)", (equipmentID, borrowerID, replacementDate))
+  db.commit()
+  mycursor.close()
+
+#Pass multiple values since compounded PK
+def delReturnedEquipment(equipmentID, borrowerID, returnedDate):
+  mycursor = db.cursor()
+
+  mycursor.execute("DELETE FROM returned_equipment WHERE (EquipmentID = %s AND BorrowerID = %s AND Return_date = %s)", (equipmentID, borrowerID, returnedDate))
   db.commit()
   mycursor.close()
