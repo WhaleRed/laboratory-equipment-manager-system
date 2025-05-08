c252d8321c7b264d1d6755218c30358e3cf09c7a
WhaleRed <redwhalecs@gmail.com>
2025-05-08 15:22:58 +0800

    Search feature for all tables

---
+++ Application/src/modules/fetchData.py
@@ -0,0 +1,133 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+
+db = mysql.connector.connect(
+  host= os.getenv("DATABASE_HOST"),
+  user = os.getenv("DATABASE_USER"),
+  password= os.getenv("DATABASE_PASSWORD"),
+  database= os.getenv("DATABASE_NAME")
+)
+
+#For Search with Pagnization
+def searchBorrower(searched, page):
+  mycursor = db.cursor()
+  
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(6):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM borrower WHERE BorrowerID LIKE %s OR ProfessorID LIKE %s OR FirstName LIKE %s OR LastName LIKE %s OR Program LIKE %s OR Block LIKE %s ORDER BY BorrowerID ASC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+  
+  mycursor.close()
+
+  return arr
+
+def searchProfessor(searched, page):
+  mycursor = db.cursor()
+
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(3):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM professor WHERE ProfessorID LIKE %s OR FirstName LIKE %s OR LastName LIKE %s ORDER BY ProfessorID ASC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+
+  mycursor.close()
+
+  return arr
+
+def searchEquipment(searched, page):
+  mycursor = db.cursor()
+
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(4):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM equipment WHERE EquipmentID LIKE %s OR Name LIKE %s OR Quantity LIKE %s OR Category LIKE %s ORDER BY EquipmentID ASC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+
+  mycursor.close()
+
+  return arr
+
+def searchBorrowedEquipment(searched, page):
+  mycursor = db.cursor()
+
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(3):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM borrowed_equipment WHERE EquipmentID LIKE %s OR BorrowerID LIKE %s OR Borrow_date LIKE %s ORDER BY Borrow_date DESC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+
+  mycursor.close()
+
+  return arr
+
+def searchReplacedEquipment(searched, page):
+  mycursor = db.cursor()
+
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(3):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM replaced_equipment WHERE EquipmentID LIKE %s OR BorrowerID LIKE %s OR Replacement_date LIKE %s ORDER BY Replacement_date DESC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+
+  mycursor.close()
+
+  return arr
+
+def searchReturnedEquipment(searched, page):
+  mycursor = db.cursor()
+
+  pattern = f"%{searched}%"
+  arr = []
+  search = []
+  for i in range(4):
+    search.append(pattern)
+  offset = (page-1) * 10
+  search.append(offset)
+
+  mycursor.execute("SELECT * FROM returned_equipment WHERE EquipmentID LIKE %s OR BorrowerID LIKE %s OR Return_date LIKE %s OR Status LIKE %s ORDER BY Return_date DESC LIMIT 10 OFFSET %s", search)
+
+  for row in mycursor:
+    arr.append(row)
+
+  mycursor.close()
+
+  return arr
