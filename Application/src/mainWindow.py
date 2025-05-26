93b4d4e5817d2b9013176ebdd6bcc6d190aa09a6
Karol <130910691+karoltacang@users.noreply.github.com>
2025-05-27 02:27:02 +0800

    fixed populateEquipmentTable and connected some input buttons

--- Application/src/mainWindow.py
+++ Application/src/mainWindow.py
@@ -10,7 +10,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-        self.setupTableBehavior()
+        #self.setupTableBehavior()
         
         self.connector = Connector(self)
         self.logic = Confirmation(self)
@@ -23,26 +23,55 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.populateReplaceTable()
         
         self.Date_box_borrow.currentIndexChanged.connect(lambda _: self.populateBorrowTable())
-        #self.Date_box_return.currentIndexChanged.connect(self.on_return_date_filter_changed)
-        #self.Date_box_replace.currentIndexChanged.connect(self.on_replace_date_filter_changed)
+        self.Date_box_return.currentIndexChanged.connect(lambda _: self.populateReturnTable())
+        self.Date_box_replace.currentIndexChanged.connect(lambda _: self.populateReplaceTable())
         
-        #self.searchbox_transaction.returnPressed.connect(self.)
-        
-        #self.sear
+        self.searchbox_transaction.returnPressed.connect(self.getCurrentTransactionTab)
+        self.searchbox_inventory.returnPressed.connect(self.populateEquipmentTable)
         
         self.arrow_right_borrow.clicked.connect(self.go_to_next_page)
 
-#-----Date Filter-----#
+#-----Helper-----#
+
+    def getCurrentTransactionTab(self):
+        
+        current_index = self.Dashboard_Frame.currentIndex()
+        
+        if current_index == 0:
+            self.populateBorrowTable()
+        elif current_index == 1:
+            self.populateReturnTable()
+        elif current_index == 2:
+            self.populateReplaceTable()
+            
+    def createOptionsButton(self, id):
+        btn = QtWidgets.QPushButton(" ⋮ ")
+        btn.setCursor(QtCore.Qt.CursorShape.PointingHandCursor)
+        
+        btn.setStyleSheet("text-align: center;")
+        #btn.setFixedSize(30, 30)
+
+        menu = QtWidgets.QMenu(btn)
+        edit_action = QtGui.QAction("Edit", self)
+        delete_action = QtGui.QAction("Delete", self)
+        menu.addAction(edit_action)
+        menu.addAction(delete_action)
+
+        edit_action.triggered.connect(partial(self.editRow, id))
+        delete_action.triggered.connect(partial(self.deleteRow, id))
+
+        btn.setMenu(menu)
+
+        return btn
+
         
 #-----Populates tables-----#
 
     def populateEquipmentTable(self):
         try:
 
-            #sortState = self.Filter_box_borrow.currentIndex() or 0
-            #dateState = self.Date_box_borrow.currentIndex() or 0
-            
-            sortState = 0 # temp
+            sortState = self.sort_box_inventory.currentIndex() or 0
+            catState = self.category_box_inventory.currentIndex() or 0
             
             searchKeyword = self.searchbox_inventory.text().strip()
             
@@ -55,20 +84,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 sortState = 0
             
             if searchKeyword:
-                data = fetchData.searchEquipmentMatch(page, sortState, searchKeyword)
+                data = fetchData.searchEquipmentMatch(page, sortState, catState, searchKeyword)
             else:
-                data = fetchData.searchEquipmentMatch(page, sortState)
+                data = fetchData.searchEquipmentMatch(page, sortState, catState)
 
             self.inventory_table.setRowCount(len(data))
             for row, item in enumerate(data):
                 self.inventory_table.setItem(row, 0, QtWidgets.QTableWidgetItem(str(item[0])))
                 self.inventory_table.setItem(row, 1, QtWidgets.QTableWidgetItem(str(item[1])))
                 self.inventory_table.setItem(row, 2, QtWidgets.QTableWidgetItem(str(item[2])))
-                self.inventory_table.setItem(row, 4, QtWidgets.QTableWidgetItem(str(item[3])))
-                self.inventory_table.setItem(row, 3, QtWidgets.QTableWidgetItem(str(item[4])))
+                self.inventory_table.setItem(row, 3, QtWidgets.QTableWidgetItem(str(item[3])))
                 
                 btn = self.createOptionsButton(item[0])
-                self.borrow_table.setCellWidget(row, 5, btn)
+                self.borrow_table.setCellWidget(row, 4, btn)
                 
         except Exception as e:
             print(f"Error in populateEquipmentTable: {e}")
@@ -169,25 +197,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         except Exception as e:
             print(f"Error in populateReplaceTable: {e}")
             
-    def createOptionsButton(self, id):
-        btn = QtWidgets.QPushButton(" ⋮ ")
-        btn.setCursor(QtCore.Qt.CursorShape.PointingHandCursor)
-        
-        btn.setStyleSheet("text-align: center;")
-        #btn.setFixedSize(30, 30)
-
-        menu = QtWidgets.QMenu(btn)
-        edit_action = QtGui.QAction("Edit", self)
-        delete_action = QtGui.QAction("Delete", self)
-        menu.addAction(edit_action)
-        menu.addAction(delete_action)
-
-        edit_action.triggered.connect(partial(self.editRow, id))
-        delete_action.triggered.connect(partial(self.deleteRow, id))
-
-        btn.setMenu(menu)
-
-        return btn
             
 #-----Edit and Delete functions-----#
 
