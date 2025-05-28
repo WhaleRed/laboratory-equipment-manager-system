9b484cdfcec9f9416e49ad95d5c114e0499b6c8b
WhaleRed <redwhalecs@gmail.com>
2025-05-28 13:09:26 +0800

    Edit for student

--- Application/src/modules/fetchData.py
+++ Application/src/modules/fetchData.py
@@ -1399,3 +1399,12 @@ def fetchProfID():
   mycursor.close()
   
   return [row[0] for row in results]
+
+def fetchBorrower(idnum):
+  mycursor = db.cursor()
+  
+  mycursor.execute("SELECT * FROM borrower WHERE BorrowerID = %s",(idnum,))
+  results = mycursor.fetchall()
+  
+  mycursor.close()
+  return results
